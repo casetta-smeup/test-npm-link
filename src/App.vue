3eaaa0f1773b99ea98a193361b47ffbb19772790
casetta-smeup <francesco.casetta@smeup.com>
2018-11-28 15:37:13 +0100

    npm link

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SmeupFld
+      id="fld1"
+      type="Itx"
+      initialValue="asd"
+      label="InputText"
+      showSubmit
+      mobile
+      clearValue
+      @change="onFldChange"
+      @click="onFldClick"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+
+  methods: {
+    onFldChange() {
+
+    },
+
+    onFldClick() {
+      
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueJsf from "vuejsf";
+
+Vue.use(VueJsf)
+
 Vue.config.productionTip = false
 
 new Vue({
